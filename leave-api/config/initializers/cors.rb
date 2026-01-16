b3ee77ed742e352e50af5a44b7826149775baa2f
Berkay Aksu <berkay@csgold.bet>
2026-01-16 18:44:54 +0300

    Update CORS

--- leave-api/config/initializers/cors.rb
+++ leave-api/config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://leave-management-system-chi-nine.vercel.app', 'http://localhost:3000'
     
     resource '*',
       headers: :any,
